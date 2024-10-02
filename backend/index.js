347dffe470a023b7e010b78337910ebd6d28494b
Mathias Fontain <mathias.fontain@vol.at>
2024-10-02 13:00:32 +0200

    update urls for backend

--- backend/index.js
+++ backend/index.js
@@ -7,33 +7,33 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/info', async (req, res) => {
+app.get('/api/info', async (req, res) => {
   res.send('Working!')
 })
 
-app.get('/users', async (req, res) => {
+app.get('/api/users', async (req, res) => {
   const users = await getUsers()
   res.send(users)
 })
 
-app.get('/countusers', async (req, res) => {
+app.get('/api/countusers', async (req, res) => {
   const users = await getUsersCount()
   res.send(users)
 })
 
-app.get('/users/:id', async (req, res) => {
+app.get('/api/users/:id', async (req, res) => {
   const id = req.params.id
   const user = await getUser(id)
   res.send(user)
 })
 
-app.post('/users', async (req, res) => {
+app.post('/api/users', async (req, res) => {
   const { username, email, password } = req.body
   const user = await createUser(username, email, password)
   res.status(201).send(user)
 })
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   console.error(err.stack)
   res.status(500).send('something went wrong')
 })
--- src/services/users.js
+++ src/services/users.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001'
+const baseUrl = 'https://nothing.uber.space/api' // TODO: LOCAL vs Production
 
 const getAll = () => {
   const request = axios.get(baseUrl)
