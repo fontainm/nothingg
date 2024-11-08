e45fc0508af3dbe5a9de4d2a6bbd18c6e4212360
Mathias Fontain <mathias.fontain@vol.at>
2024-11-08 08:27:42 +0100

    add users table too

--- backend/tests/setup_test_db.sql
+++ backend/tests/setup_test_db.sql
@@ -5,3 +5,15 @@ CREATE TABLE IF NOT EXISTS products (
 );
 
 INSERT INTO products (title, price) VALUES ('Nothing', 0), ('Nothing+', 1.00);
+
+CREATE TABLE IF NOT EXISTS users (
+  id BIGINT PRIMARY KEY,
+  username VARCHAR(50) NOT NULL,
+  password TEXT NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  confirmed BOOLEAN DEFAULT false,
+  product_id INTEGER DEFAULT 1 NOT NULL,
+  CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES products(id),
+  CONSTRAINT unique_username UNIQUE (username)  -- Table-level UNIQUE constraint
+);
